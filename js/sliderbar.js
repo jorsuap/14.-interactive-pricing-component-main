7fa809dfd16609345faab517e13a33ca96222882
jorsuap <jorsuap@gmail.com>
2021-08-19 15:42:18 -0500

    master

--- js/sliderbar.js
+++ js/sliderbar.js
@@ -1,9 +1,22 @@
-var slider = document.getElementById("myRange");
+var slider = document.getElementById("range");
 var output = document.getElementsByClassName("price-permonth")[0];
-output.innerHTML = parseFloat(slider.value);
+var pages = document.getElementsByClassName("pages-permonth")[0];
+output.innerHTML = "$" + slider.value + ".00";
+pages.innerHTML = slider.value * 6.25 + " K";
+
+slider.style.background = "linear-gradient(to right, hsl(174, 77%, 80%) " + slider.value / 32 * 100 +
+   "%, hsl(224, 65%, 95%) " + slider.value / 32 * 100 + "%)";
 
 slider.oninput = function () {
-   output.innerHTML = "$ " + this.value;
+   if(slider.value > 16){
+      pages.innerHTML = parseInt(slider.value * 15.63) + " K";
+   }
+   else{
+      pages.innerHTML = parseInt(slider.value * 6.25) + " K";
+   }
+   output.innerHTML = "$ " + ((this.value / 32) * 32).toFixed(2);
    rango = output.innerHTML;
-   console.log(rango.tyof);
+   slider.style.background = "linear-gradient(to right, hsl(174, 77%, 80%) " + slider.value / 32 * 100 +
+      "%, hsl(224, 65%, 95%) " + slider.value / 32 * 100 + "%)";
+   console.log(rango);
 }
