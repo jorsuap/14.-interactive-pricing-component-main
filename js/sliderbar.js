3cf2d8282c8e0e9fe0f35724bc5da6b386822073
jorsuap <jorsuap@gmail.com>
2021-08-19 11:25:04 -0500

    master

--- js/sliderbar.js
+++ js/sliderbar.js
@@ -3,7 +3,7 @@ var output = document.getElementsByClassName("price-permonth")[0];
 output.innerHTML = parseFloat(slider.value);
 
 slider.oninput = function () {
-   output.innerHTML = "$ " + this.value ;
+   output.innerHTML = "$ " + this.value;
    rango = output.innerHTML;
    console.log(rango.tyof);
 }
