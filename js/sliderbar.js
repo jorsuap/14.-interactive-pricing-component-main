ac5c02ba03d6923fe4286be2e0ef446d68413012
jorsuap <jorsuap@gmail.com>
2021-08-19 20:20:06 -0500

    master

--- js/sliderbar.js
+++ js/sliderbar.js
@@ -1,9 +1,11 @@
 var slider = document.getElementById("range");
 var output = document.getElementsByClassName("price-permonth")[0];
+var output1 = document.getElementsByClassName("price-permonth")[1];
 var pages = document.getElementsByClassName("pages-permonth")[0];
 var checkbox = document.getElementsByClassName("toggle")[0];
 var year = document.getElementsByClassName("month")[0];
 output.innerHTML = "$" + slider.value + ".00";
+output1.innerHTML = "$" + slider.value + ".00";
 pages.innerHTML = slider.value * 6.25 + " K";
 var descuesnto = 0;
 var rango = 16;
@@ -16,11 +18,13 @@ checkbox.addEventListener('change', function () {
       descuesnto = 1;
       year.innerHTML = " /year";
       output.innerHTML = "$ " + ((slider.value * 12) - (slider.value * 12 * 0.25)).toFixed(2);
+      output1.innerHTML = "$ " + ((slider.value * 12) - (slider.value * 12 * 0.25)).toFixed(2);
    }
    else{
       year.innerHTML = "  /month";
       descuesnto = 0;
       output.innerHTML = "$ " + ((slider.value / 32) * 32).toFixed(2);
+      output1.innerHTML = "$ " + ((slider.value / 32) * 32).toFixed(2);
    }
 });
 
@@ -29,10 +33,14 @@ slider.oninput = function () {
    if(descuesnto == 1){
       output.innerHTML = "$ " + ((this.value * 12) - (this.value * 12 * 0.25)).toFixed(2);
       rango = output.innerHTML;
+      output1.innerHTML = "$ " + ((this.value * 12) - (this.value * 12 * 0.25)).toFixed(2);
+      rango = output1.innerHTML;
    }
    else{
       output.innerHTML = "$ " + ((this.value / 32) * 32).toFixed(2);
       rango = output.innerHTML;
+      output1.innerHTML = "$ " + ((this.value / 32) * 32).toFixed(2);
+      rango = output1.innerHTML;
    }
    if (slider.value > 16) {
       pages.innerHTML = parseInt(slider.value * 15.63) + " K";
